df56cbe3928770ba566ddded9eb7bde46e98804b
kotathefriends <5070kouta@gmail.com>
2023-12-02 16:35:18 +0900

    Initial Commit

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by KOTA TAKAHASHI on 2023/12/02.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
